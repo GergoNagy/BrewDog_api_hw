3a5df91bfca98dbad9c86f58362e9f096058c849
Gergo Nagy <guzmop@gmail.com>
2017-09-06 21:57:38 +0100

    show the ingredients, clear the list when pop the new one

--- public/index.js
+++ public/index.js
@@ -2,9 +2,11 @@ window.addEventListener('load', function(){
     var url = "https://api.punkapi.com/v2/beers"
 
     makeRequest(url, function(){
+        if (this.status != 200) return alert("error: Too Many Requests")
         var jsonString = this.responseText;
         var beers = JSON.parse(jsonString);
         dropdown(beers);
+        
     })
 });
 
@@ -31,10 +33,54 @@ var dropdown = function(beers){
         var beer = beers[index];
 
         updateInfo(beer)
-
+        
         var jsonString = JSON.stringify(beer);
         localStorage.setItem('selectedBeer', jsonString);
     })
+}
+
+
+var selectMalt = function(beer){
+    $('#list').empty();
+    var ul = document.querySelector('#list');
+    var h = document.createElement('h');
+    h.innerHTML = "Malt: "
+    ul.appendChild(h)
+
+    beer.malt.forEach(function(item){
+        var li = document.createElement('li');
+        li.innerText = item.name + ": " + item.amount.value + " " + item.amount.unit;
+        ul.appendChild(li)
+    })
+}
+
+var selectHops = function (beer) {
+    $('#list1').empty();
+    var ul = document.querySelector('#list1');
+    var h = document.createElement('h');
+    h.innerText = "Hops: "
+    ul.appendChild(h)
+
+    beer.hops.forEach(function (item) {
+        var li = document.createElement('li');
+        li.innerText = item.name + ": " + item.amount.value + " " + item.amount.unit;
+        var li1 = document.createElement('li');
+        li1.innerText = "Added in " + item.add + " to give " + item.attribute;
+        ul.appendChild(li)
+        ul.appendChild(li1)
+    })
+}
+
+var selectYeast = function (beer) {
+    $('#list2').empty();
+    var ul = document.querySelector('#list2');
+    var h = document.createElement('h');
+    h.innerText = "Yeast: "
+    ul.appendChild(h)
+
+        var li = document.createElement('li');
+        li.innerText = beer.yeast
+        ul.appendChild(li)
 }
 
 var render = function(beers){
@@ -54,8 +100,15 @@ var updateInfo = function(beer){
 
     var pTag = document.querySelectorAll('#info p');
     pTag[0].innerText = beer.name;
-    pTag[1].innerText = beer.tagline;
-    pTag[2].innerText = beer.description;
+    pTag[1].innerText = beer.abv;
+    pTag[2].innerText = beer.tagline;
+    pTag[3].innerText = beer.description;
+    pTag[4].innerText = beer.food_pairing;
+    pTag[5].innerText = beer.brewers_tips;
+
+    selectMalt(beer.ingredients);
+    selectYeast(beer.ingredients);
+    selectHops(beer.ingredients);
 
 }
 
